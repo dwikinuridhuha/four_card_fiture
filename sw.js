9ae7c56f16b2da79bf01b881f3f6467096296b00
dwiki nuridhuha <dwiki.nuridhuha@gmail.com>
2020-12-28 10:22:18 +0700

    menambah fallback

--- sw.js
+++ sw.js
@@ -1,4 +1,9 @@
 const dinamisCache = "dinammis-v1";
+const statikCache = "static-v1"
+
+const assets = [
+  '/fallback.html'
+];
 
 // cache size limit function
 const limitCacheSize = (name, size) => {
@@ -14,16 +19,23 @@ const limitCacheSize = (name, size) => {
 // install event
 self.addEventListener('install', evt => {
   console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(statikCache).then((cache) => {
+      // console.log('caching shell assets');
+      // console.log(assets);
+      cache.addAll(assets);
+    })
+  );
 });
 
 // activate event
 self.addEventListener('activate', evt => {
-  //console.log('service worker activated');
+  console.log('service worker activated');
   evt.waitUntil(
     caches.keys().then(keys => {
       //console.log(keys);
       return Promise.all(keys
-        .filter(key => key !== dinamisCache)
+        .filter(key => key !== statikCache && key !== dinamisCache)
         .map(key => caches.delete(key))
       );
     })
@@ -32,22 +44,20 @@ self.addEventListener('activate', evt => {
 
 // fetch events
 self.addEventListener('fetch', evt => {
-  if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
-    evt.respondWith(
-      caches.match(evt.request).then(cacheRes => {
-        return cacheRes || fetch(evt.request).then(fetchRes => {
-          return caches.open(dinamisCache).then(cache => {
-            cache.put(evt.request.url, fetchRes.clone());
-            // check cached items size
-            limitCacheSize(dinamisCache, 15);
-            return fetchRes;
-          })
-        });
-      }).catch(() => {
-        if(evt.request.url.indexOf('.html') > -1){
-          return caches.match('/pages/fallback.html');
-        } 
-      })
-    );
-  }
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dinamisCache).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          // check cached items size
+          limitCacheSize(dinamisCache, 15);
+          return fetchRes;
+        })
+      });
+    }).catch(() => {
+      if(evt.request.url.indexOf('.html') > -1){
+        return caches.match('/fallback.html');
+      } 
+    })
+  );
 });
